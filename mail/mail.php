6d98d575aded85cdf2827b26d18fc494c80e3c8d
kingqabil <kingqabil@live.com>
2021-11-06 19:58:56 +0300

    Added index css js files

---
+++ mail/mail.php
@@ -0,0 +1,16 @@
+<?
+$name = $_POST{'name'};
+$email = $_POST{'email'};
+$message = $_POST['message'];
+
+$email_message = "
+
+Name: ".$name."
+Email: ".$email."
+Message: ".$message."
+
+";
+
+mail ("kingqabil@gmail.com" , "New Message", $email_message);
+header("location: ../mail-success.html ");
+?>
